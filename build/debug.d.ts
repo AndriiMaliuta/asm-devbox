0be4fea8d9d1a38739b1f5ee6b04bae4e444389d
Andrii Maliuta <quadr988@gmail.com>
2023-04-15 15:15:35 +0300

    arrays

---
+++ build/debug.d.ts
@@ -0,0 +1,7 @@
+/** Exported memory */
+export declare const memory: WebAssembly.Memory;
+/**
+ * assembly/index/getData
+ * @returns `~lib/typedarray/Int32Array`
+ */
+export declare function getData(): Int32Array;
